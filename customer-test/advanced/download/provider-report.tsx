a16e63c7f9d126f9987404bb8f4314fbb7a2e4e2
1n1lha <1n1lha@users.noreply.github.com>
2025-11-03 16:09:38 +0000

    Add provider-report.tsx, backend-main.js

---
+++ customer-test/advanced/download/provider-report.tsx
@@ -0,0 +1,5 @@
+export const config = { runtime: 'edge' };
+
+export const usecommon_manager_message = () => { return {}; };
+
+
---
+++ customer-test/backend-main.js
@@ -0,0 +1,5 @@
+import axios from 'axios';
+
+export const components_helpers_factory = () => { return null; };
+
+
